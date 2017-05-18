6b50d266c25b7f25c5bc7ef1926b190505264607
Philippe Veber <philippe.veber@gmail.com>
2017-05-19 01:25:19 +0200

    Client: changing fields updates state!

--- app/bistro_client_app.ml
+++ app/bistro_client_app.ml
@@ -42,45 +42,65 @@ let br () = Vdom.elt "br" []
 
 let label ?a xs = Vdom.elt ?a "label" xs
 
-let rec form_view ?legend { fields } =
-  form [ fieldset ?legend @@ List.flat_map fields ~f:field_view ]
+let rec assoc_replace xs k v =
+  match xs with
+  | [] -> assert false
+  | ((k', _) as p) :: t ->
+    if k = k' then (k, v) :: t
+    else p :: assoc_replace t k v
 
-and field_view =
+let rec form_view ?legend k { fields } =
+  let f (label, field_kind) =
+    let k x = k { fields = assoc_replace fields label x } in
+    field_view k label field_kind
+  in
+  form [ fieldset ?legend @@ List.flat_map fields ~f ]
+
+and field_view k lab =
   let open Vdom in
   function
-  | lab, Int_field value -> [
+  | Int_field value -> [
       label [text lab] ;
       (
         let a =
           List.cons_maybe
             Option.(map (string_of_int % attr "value") value)
-            [attr "type" "number"]
+            [
+              attr "type" "number" ;
+              oninput (fun i -> k (Int_field (Some (int_of_string i)))) ;
+            ]
         in
         input ~a []
       ) ;
       br () ;
     ]
-  | lab, String_field value -> [
+  | String_field value -> [
       label [text lab] ;
       (
-        let a = Option.(to_list (map (attr "value") value)) in
+        let a =
+          List.cons_maybe
+            Option.(map (attr "value") value)
+            [
+              oninput (fun s -> k (String_field (Some s))) ;
+            ]
+        in
         input ~a []
       ) ;
       br () ;
     ]
-  | lab, Form_field f -> [
-      form_view ~legend:lab f ; br ()
-    ]
+  | Form_field f ->
+    let k x = k (Form_field x) in
+    [ form_view ~legend:lab k f ; br () ]
 
-let update m () =
-  m, Vdom.Cmd.Batch []
+let update m app_form =
+  { m with app_form }, Vdom.Cmd.Batch []
 
 let view spec =
   let open Vdom in
   div ~a:[attr "class" "container"] [
     h2 [ text spec.app_title ] ;
     br () ;
-    form_view spec.app_form ;
+    form_view (fun x -> x) spec.app_form ;
     text @@ Sexplib.Sexp.to_string_hum @@ sexp_of_form spec.app_form ;
   ]
 
